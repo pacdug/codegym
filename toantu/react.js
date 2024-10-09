25ebb280be16d09bdd593a6d4cd2bac300879b3d
pacdug <buitiendung091003@gmail.com>
2024-10-09 09:00:43 +0700

    Add files via upload

---
+++ toantu/react.js
@@ -0,0 +1,10 @@
+
+inputwidth=prompt('nhap chieu rong : ');
+inputheight=prompt('nhap chieu dai : ');
+
+let width=parseInt(inputwidth);
+let height=parseInt(inputheight);
+
+let area = width * height;
+
+document.write('dien tich hinh chu nhat la : ' + area);
